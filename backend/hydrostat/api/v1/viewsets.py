3595d9e7a91c324f9017b84c9c93b78fe90e33f4
zevrector21 <zevrector21@gmail.com>
2020-12-05 06:12:24 -0500

    add file upload

--- backend/hydrostat/api/v1/viewsets.py
+++ backend/hydrostat/api/v1/viewsets.py
@@ -33,37 +33,39 @@ class UploadFileView(ViewSet):
     def create(self, request):
         if 'file' not in request.data:
             return Response(status=400)
-
-        file_in_memory = request.data['file'].read()
-        wb = load_workbook(filename=BytesIO(file_in_memory), data_only=True).active
-        
-        for idx, row in enumerate(wb.rows):            
-            if idx == 0 or row[0].value == None:
-                continue
-            try:
-                column = Columns.objects.create(
-                    bin=row[0].value,
-                    noun=row[1].value, 
-                    orgshop=row[2].value, 
-                    status=row[3].value, 
-                    notes=row[4].value, 
-                    discrepacies=row[5].value, 
-                    micap=True if row[6].value == 'YES' else False,
-                    dateIn=row[7].value, 
-                    dateOut=row[8].value, 
-                    initiateDate=row[9].value, 
-                    enditemDocnumber=row[10].value, 
-                    dueoutpieces=row[11].value, 
-                    dueOutDocnumbers=row[12].value, 
-                    difm=row[13].value, 
-                    incomingserialnumber=row[14].value, 
-                    jobcontrolnumber=row[15].value, 
-                    tailnumber=row[16].value, 
-                    three50tag=row[17].value, 
-                    outgoingserialnumber=row[18].value, 
-                    sevenLevel=row[19].value, 
-                )
-            except:
-                pass
+        try:
+            file_in_memory = request.data['file'].read()
+            wb = load_workbook(filename=BytesIO(file_in_memory), data_only=True).active
+            
+            for idx, row in enumerate(wb.rows):            
+                if idx == 0 or row[0].value == None:
+                    continue
+                try:
+                    column = Columns.objects.create(
+                        bin=row[0].value,
+                        noun=row[1].value, 
+                        orgshop=row[2].value, 
+                        status=row[3].value, 
+                        notes=row[4].value, 
+                        discrepacies=row[5].value, 
+                        micap=True if row[6].value == 'YES' else False,
+                        dateIn=row[7].value, 
+                        dateOut=row[8].value, 
+                        initiateDate=row[9].value, 
+                        enditemDocnumber=row[10].value, 
+                        dueoutpieces=row[11].value, 
+                        dueOutDocnumbers=row[12].value, 
+                        difm=row[13].value, 
+                        incomingserialnumber=row[14].value, 
+                        jobcontrolnumber=row[15].value, 
+                        tailnumber=row[16].value, 
+                        three50tag=row[17].value, 
+                        outgoingserialnumber=row[18].value, 
+                        sevenLevel=row[19].value, 
+                    )
+                except:
+                    pass
+        except:
+            pass
         return Response(status=200)
 
